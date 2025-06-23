505359dcaa8cee7189f0217a0f1a7b1b982f5542
LakshmanYadavilli <lakshmanyadavilli2001@gmail.com>
2025-06-23 22:21:43 +0530

    Episode-02

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parentElement = React.createElement(
   "div",
   { id: "parent" },
-  React.createElement("div", { id: "children" }, [
-    React.createElement("h1", { id: "heading" }, "Hello World!"),
-    React.createElement("p", { id: "paragraph" }, "This is a paragraph"),
-  ])
+  React.createElement(
+    "div",
+    { id: "children" },
+    React.createElement("h1", { id: "heading" }, "Hello World!")
+  )
 );
 // const h1Element = React.createElement("h1", { id: "heading" }, "Hello World!");
 const rootElement = document.getElementById("root");
